7b48b7a6fd9fb37ab3e91034271cf7b56b07d186
mendelsshop <mendelsshop@gmail.com>
2024-01-21 21:56:05 -0500

    done most of p3

---
+++ 3-memory-and-computer1/src/Processor.java
@@ -0,0 +1,35 @@
+public class Processor {
+    private Word PC = new Word(new Bit[32]);
+    private Word SP = new Word(new Bit[32]);
+    private Word currentInstruction = new Word(new Bit[32]);
+    private Bit halted = new Bit(false);
+
+    public Processor() {
+        PC.set(0);
+        SP.set(1024);
+    }
+
+    private void decode() {
+    }
+
+    private void execute() {
+    }
+
+    private void store() {
+    }
+
+    private void fetch() {
+        currentInstruction = MainMemory.read(PC);
+        // TODO: is increment in place?
+        PC = PC.increment();
+    }
+
+    public void run() {
+        while (halted.not().getValue()) {
+            fetch();
+            decode();
+            execute();
+            store();
+        }
+    }
+}
