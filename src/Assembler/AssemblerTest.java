e7b3c08ec803d8d4bc45fb05f1a6354bf3d3759c
mendelsshop <mendelsshop@gmail.com>
2024-04-16 09:17:25 -0400

    [p6] lots of assembler tests

--- src/Assembler/AssemblerTest.java
+++ src/Assembler/AssemblerTest.java
@@ -4,71 +4,89 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.util.LinkedList;
 
 import Assembler.Instruction.RegisterFormat;
 
 public class AssemblerTest {
-    public static void main(String[] args) {
-        if (args.length != 3) {
-            System.err.println("Usage Assembler [input] [output]");
-            System.exit(1);
-        }
-        // get the paths
-        Path myPath = Paths.get(args[1]);
-        Path myOutPath = Paths.get(args[2]);
-        String content;
-        try {
-            // read the assembly
-            content = new String(Files.readAllBytes(myPath));
+	public static void main(String[] args) {
+		if (args.length != 3) {
+			System.err.println("Usage Assembler [input] [output]");
+			System.exit(1);
+		}
+		// get the paths
+		Path myPath = Paths.get(args[1]);
+		String content;
+		try {
+			// read the assembly
+			content = new String(Files.readAllBytes(myPath));
 
-            try {
-                // lex the assembly for tokens
-                Lexer lexer = new Lexer(content);
-                LinkedList<Token> lexed = lexer.lex();
+			try {
+				// lex the assembly for tokens
+				Lexer lexer = new Lexer(content);
+				LinkedList<Token> lexed = lexer.lex();
+				System.out.println("@Test");
+				System.out.println("public void " + args[1].substring(0, args[1].length() - 4) + "() throws Exception {");
+				
+				System.out.print("assemblerTester(\"\"\"\n" + content + "\"\"\", new Token[] {");
+				for (var token : lexed) {
+					System.out.print("new Token(" + token.getStartPosition() + ", "
+							+ token.getLineNumber()
+							+ ", Token.TokenType." + token.getType()
+							+ token.getValue().map(val -> ", " + val).orElse("") + "), ");
 
-                System.out.print("\"\"\"\n" + content + "\"\"\", new Token[] {");
-                for (var token : lexed) {
-                    System.out.print("new Token(" + token.getStartPosition() + ", " + token.getLineNumber()
-                            + ", Token.TokenType." + token.getType()
-                            + token.getValue().map(val -> ", " + val).orElse("") + "), ");
+				}
+				System.out.println("},");
+				// parse and output to file
+				var parser = new Parser(lexed);
+				LinkedList<Instruction> parsed = parser.parse().get();
 
-                }
-                System.out.println("},");
-                // parse and output to file
-                var parser = new Parser(lexed);
-                LinkedList<Instruction> parsed = parser.parse().get();
+				System.out.print("new Instruction[] {");
+				for (var inst : parsed) {
+					System.out
+							.println("Instruction.buildWithType(Instruction.InstructionType."
+									+ inst.getType() + ").immediate("
+									+ inst.getImmediate()
+									+ ").operation(Instruction.Operation."
+									+ inst.getOperation()
+									+ ").registers(new Instruction.RegisterFormat."
+									+ switch (inst.getRegisters()) {
+										case RegisterFormat.NoR() -> "NoR()),";
+										case RegisterFormat.DestOnly(RegisterFormat.Register rd) ->
+											"DestOnly(new Instruction.RegisterFormat.Register("
+													+ rd.number()
+													+ " ))),";
+										case RegisterFormat.TwoR(RegisterFormat.Register rs1, RegisterFormat.Register rd) ->
+											"TwoR(new Instruction.RegisterFormat.Register("
+													+ rs1.number()
+													+ " ), new Instruction.RegisterFormat.Register("
+													+ rd.number()
+													+ " ))),";
+										case RegisterFormat.ThreeR(RegisterFormat.Register rs1, RegisterFormat.Register rs2, RegisterFormat.Register rd) ->
+											"ThreeR( new Instruction.RegisterFormat.Register("
+													+ rs1.number()
+													+ " ), new Instruction.RegisterFormat.Register("
+													+ rs2.number()
+													+ " ), new Instruction.RegisterFormat.Register("
+													+ rd.number()
+													+ " ))),";
 
-                System.out.print("new Instruction[] {");
-                for (var inst : parsed) {
-                    System.out.println("Instruction.buildWithType(Instruction.InstructionType." + inst.getType()
-                            + ").immediate(" + inst.getImmediate() + ").operation(Instruction.Operation."
-                            + inst.getOperation() + ").registers(new Instruction.RegisterFormat."
-                            + switch (inst.getRegisters()) {
-                                case RegisterFormat.NoR() -> "NoR()),";
-                                case RegisterFormat.DestOnly(RegisterFormat.Register rd) ->
-                                    "DestOnly(new Instruction.RegisterFormat.Register(" + rd.number() + " ))),";
-                                case RegisterFormat.TwoR(RegisterFormat.Register rs1, RegisterFormat.Register rd) ->
-                                    "TwoR(new Instruction.RegisterFormat.Register(" + rs1.number() + " ), new Instruction.RegisterFormat.Register(" + rd.number() + " ))),";
-                                case RegisterFormat.ThreeR(RegisterFormat.Register rs1, RegisterFormat.Register rs2, RegisterFormat.Register rd) ->
-                                    "ThreeR( new Instruction.RegisterFormat.Register(" + rs1.number() + " ), new Instruction.RegisterFormat.Register(" + rs2.number() + " ), new Instruction.RegisterFormat.Register(" + rd.number() + " ))),";
+									});
 
-                            });
+				}
+				System.out.println("},");
+				System.out.print("new String[] {");
+				for (var inst : parsed) {
+					System.out.print("\"" + inst.toBitPattern() + "\", ");
+				}
+				System.out.println("});\n}");
+			} catch (AssemblerException e) {
+				e.DisplayError(content, myPath.toString());
+			}
+		} catch (IOException e) {
+			System.err.println(
+					"IO Error while reading input file or writing output file: " + e.getMessage());
 
-                }
-                System.out.println("},");
-                System.out.print("new String[] {");
-                for (var inst : parsed) {
-                    System.out.print("\"" +inst.toBitPattern() + "\", ");
-                }
-                System.out.println("}");
-            } catch (AssemblerException e) {
-                e.DisplayError(content, myPath.toString());
-            }
-        } catch (IOException e) {
-            System.err.println("IO Error while reading input file or writing output file: " + e.getMessage());
-
-        }
-    }
+		}
+	}
 }
--- src/Assembler/Token.java
+++ src/Assembler/Token.java
@@ -3,7 +3,7 @@ package Assembler;
 import java.util.Optional;
 
 public class Token {
-    @Override
+	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
@@ -32,54 +32,54 @@ public class Token {
 	}
 
 	public enum TokenType {
-        MATH, ADD, SUBTRACT, MULTIPLY, AND, OR, NOT, XOR, COPY, HALT, BRANCH, JUMP, CALL, PUSH, LOAD, RETURN, STORE,
-        PEEK, POP, INTERRUPT, EQUAL, UNEQUAL, GREATER, LESS, GREATEROREQUAL, LESSOREQUAL, REGISTER,
-        VALUE, NEWLINE, CALLIF, RSHIFT, LSHIFT
-    }
+		MATH, ADD, SUBTRACT, MULTIPLY, AND, OR, NOT, XOR, COPY, HALT, BRANCH, JUMP, CALL, PUSH, LOAD, RETURN, STORE,
+		PEEK, POP, INTERRUPT, EQUAL, UNEQUAL, GREATER, LESS, GREATEROREQUAL, LESSOREQUAL, REGISTER, VALUE, NEWLINE,
+		CALLIF, RSHIFT, LSHIFT
+	}
 
-    private int startPosition;
+	private int startPosition;
 
-    private int lineNumber;
+	private int lineNumber;
 
-    private TokenType type;
+	private TokenType type;
 
-    // we only have integers because the only things we stores values are registers
-    // (1-32) or (0-31) (probably) and values (positive integers)
-    private Optional<Integer> value = Optional.empty();
+	// we only have integers because the only things we stores values are registers
+	// (1-32) or (0-31) (probably) and values (positive integers)
+	private Optional<Integer> value = Optional.empty();
 
-    public Token(int position, int line, TokenType type, Integer value) {
-        startPosition = position;
-        lineNumber = line;
-        this.type = type;
-        this.value = Optional.ofNullable(value);
-    }
+	public Token(int position, int line, TokenType type, Integer value) {
+		startPosition = position;
+		lineNumber = line;
+		this.type = type;
+		this.value = Optional.ofNullable(value);
+	}
 
-    public Token(int position, int line, TokenType type) {
-        startPosition = position;
-        lineNumber = line;
-        this.type = type;
-    }
+	public Token(int position, int line, TokenType type) {
+		startPosition = position;
+		lineNumber = line;
+		this.type = type;
+	}
 
-    public int getStartPosition() {
-        return startPosition;
-    }
+	public int getStartPosition() {
+		return startPosition;
+	}
 
-    public int getLineNumber() {
-        return lineNumber;
-    }
+	public int getLineNumber() {
+		return lineNumber;
+	}
 
-    public TokenType getType() {
-        return type;
-    }
+	public TokenType getType() {
+		return type;
+	}
 
-    // get the value of this token only works for registers and numbers
-    public Optional<Integer> getValue() {
-        return value;
-    }
+	// get the value of this token only works for registers and numbers
+	public Optional<Integer> getValue() {
+		return value;
+	}
 
-    @Override
-    public String toString() {
-        return type + value.map(c -> "(" + c + ")").orElse("") + "[" + startPosition + " " + lineNumber + "]";
-    }
+	@Override
+	public String toString() {
+		return type + value.map(c -> "(" + c + ")").orElse("") + "[" + startPosition + " " + lineNumber + "]";
+	}
 
 }
